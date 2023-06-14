f558cf88849062f9316eefb11435554869501b1a
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-06-14 11:44:56 -0400

    Added Navigation

--- web-portfolio/src/App.js
+++ web-portfolio/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './Components/Navigation/Nav'
+
+
 import Home from './Pages/Home'
 import Projects from './Pages/Projects';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav></Nav>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </Router>
     </div>
---
+++ web-portfolio/src/Components/Navigation/Nav.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import './Nav.css'
+
+import {Link, useLocation} from'react-router-dom';
+import logo from '../../Images/aribelLogo.jpg';
+
+
+const Nav = () => {
+
+    const [expandNavbar, setExpandNavbar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false);
+    }, [location])
+    
+    return (
+        <div className='navbar bg-light' id={expandNavbar? "open" : "close"}>
+            <div className='nav-header'>
+                <a class='navbar-brand' href='/'>
+                    <img className='brand-img' src={logo} alt='aribel-logo'></img>
+                </a>
+                <button className='navbar-toggler' id='toggle-btn' type='button' onClick={() => {setExpandNavbar((prev) => !prev)}}>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+            </div>
+            <div className='nav-body'>
+                <Link className='nav-item' to='/'> 
+                    <a className='nav-link'> Home </a>
+                </Link>
+                <Link className='nav-item' to='/projects'> 
+                    <a className='nav-link'> Projects </a> 
+                </Link>
+                <Link className='nav-item contact-btn' to='/contact'> 
+                    <a> Contact Me </a>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
