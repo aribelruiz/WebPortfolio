ae8ea2e2f408fd951544a369c4004880f3962077
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-06-15 16:09:57 -0400

    Updated Skills

--- web-portfolio/src/Components/Home/HomeSections/Skills.js
+++ web-portfolio/src/Components/Home/HomeSections/Skills.js
@@ -1,84 +1,125 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
+import jsImg from '../../../Images/Icons/javascript.png'; 
+import tsImg from '../../../Images/Icons/typescript.png';
+import htmlImg from '../../../Images/Icons/html.png';
+import cssImg from '../../../Images/Icons/css.png'; 
+
+import reactImg from '../../../Images/Icons/react.png'; 
+import bootstrapImg from '../../../Images/Icons/bootstrap.png'; 
+import figmaImg from '../../../Images/Icons/figma.png';
+
+import javaImg from '../../../Images/Icons/java.png';
+import cImg from '../../../Images/Icons/c.png';
+import cplusImg from '../../../Images/Icons/cplus.png';
+
+import gitImg from '../../../Images/Icons/github.png';
+import vsImg from '../../../Images/Icons/vscode.png';
+import unityImg from '../../../Images/Icons/unity.png';
+
+
+
+
+
+
 const Skills = () => {
     return (
         <>
             <div className='home-div skills' >
                 <div className='skills-header'>
                     <h1 className='heading'> Skills </h1>
-                    <h5 className='subheading'>My Technical Skills</h5>
+                    <h5 className='subheading'>My Top Technical Skills</h5>
                 </div>
                 <div className='skills-body'>
                     <div className='skills-item'>
                         <div className='skills-txt'>
-                            <FontAwesomeIcon className='skills-icon' icon={['fas', 'laptop-code']}/>
-                            <h2 className='subheading'> Languages </h2>
+                            <div className='skills-item-header'>
+                                <FontAwesomeIcon className='skills-icon' icon={['fas', 'laptop-code']}/>
+                                <h2 className='subheading'> Front-End </h2>
+                            </div>
                             <br></br>
-                            <h5 style={{textAlign:'left'}}>Front-End</h5>
-                            <ul className='skills-list list-bg txt-body'>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon className='skill-check' icon={['fab', 'js']}/>
-                                    <h6 className=' skill-txt txt-body'> JavaScript </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon className='skill-check' icon={['fas', 'square-check']}/>
-                                    <h6 className='skill-txt txt-body'> TypeScript </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon className='skill-check' icon={['fab', 'html5']}/>
-                                    <h6 className='skill-txt txt-body'> HTML </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon className='skill-check' icon={['fab', 'css3-alt']}/>
-                                    <h6 className='skill-txt txt-body'> CSS </h6>
-                                </div>
-                            </ul>
-                            <h5 style={{textAlign:'left'}}>Programming</h5>
-                            <ul className='skills-list list-bg txt-body'>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon icon={['fas', 'circle-check']}/>
-                                    <h6 className='skill-txt subheading'> Java </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon icon={['fas', 'circle-check']}/>
-                                    <h6 className='skill-txt txt-body'> C </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon icon={['fas', 'circle-check']}/>
-                                    <h6 className='skill-txt txt-body'> Python </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon icon={['fas', 'circle-check']}/>
-                                    <h6 className='skill-txt txt-body'> C++ </h6>
-                                </div>
-                                <div className='skill'> 
-                                    <FontAwesomeIcon icon={['fas', 'circle-check']}/>
-                                    <h6 className='skill-txt txt-body'> C# </h6>
-                                </div>
-                            </ul>
+                            <div className='skills-item-body'>                 
+                                <h5 style={{textAlign:'left'}}>Languages</h5>
+                                 <ul className='skills-list list-bg txt-body'>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={jsImg}></img>
+                                        <h6 className='subheading'> JavaScript </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={tsImg}></img>
+                                        <h6 className='subheading'> TypeScript </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={htmlImg}></img>
+                                        <h6 className='subheading'> HTML </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={cssImg}></img>
+                                        <h6 className='subheading'> CSS </h6>
+                                    </div>
+                                </ul>
+                                <h5 style={{textAlign:'left'}}>Frameworks & Tools</h5>
+                                <ul className='skills-list list-bg txt-body'>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={reactImg}></img>
+                                        <h6 className='subheading'> React </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={bootstrapImg}></img>
+                                        <h6 className='subheading'> Bootstrap </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={figmaImg}></img>
+                                        <h6 className='subheading'> Figma </h6>
+                                    </div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                 
                     <div className='skills-item'>
                         <div className='skills-txt'>
-                            <h2 className='subheading'> Skills List 1 </h2>
+                            <div className='skills-item-header'>
+                                <FontAwesomeIcon className='skills-icon' icon={['fas', 'file-code']}/>
+                                <h2 className='subheading'> Programming </h2>
+                            </div>
                             <br></br>
-                            <ul className='skills-list txt-body'>
-                                <div> 1 point </div>
-                                <div> 2 points </div>
-                            </ul>
+                            <div className='skills-item-body'>                 
+                                <h5 style={{textAlign:'left'}}>Languages</h5>
+                                 <ul className='skills-list list-bg txt-body'>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={javaImg}></img>
+                                        <h6 className='subheading'> Java </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={cImg}></img>
+                                        <h6 className='subheading'> C </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={cplusImg}></img>
+                                        <h6 className='subheading'> C++ </h6>
+                                    </div>
+                                </ul>
+                                <h5 style={{textAlign:'left'}}>Technologies & Tools</h5>
+                                <ul className='skills-list list-bg txt-body'>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={gitImg}></img>
+                                        <h6 className='subheading'> GitHub </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={vsImg}></img>
+                                        <h6 className='subheading'> VS Code </h6>
+                                    </div>
+                                    <div className='skill skill-icon'> 
+                                        <img className='icon-img' src={unityImg}></img>
+                                        <h6 className='subheading'> Unity </h6>
+                                    </div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className='skills-item'>
-                        <div className='skills-txt'>
-                            <h2 className='subheading'> Skills List 1 </h2>
-                            <br></br>
-                            <ul className='skills-list txt-body'>
-                                <div> 1 point </div>
-                                <div> 2 points </div>
-                            </ul>
-                        </div>
-                    </div>
+                
                 </div>
             </div>
         </>
--- web-portfolio/src/index.js
+++ web-portfolio/src/index.js
@@ -7,10 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-import { faEnvelope, faFolder, faLaptopCode, faSquareCheck, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faFolder, faLaptopCode, faSquareCheck, faCircleCheck, faFileCode } from '@fortawesome/free-solid-svg-icons';
 
 // Adding icons to global library
-library.add(fab, faEnvelope, faFolder, faLaptopCode, faSquareCheck, faCircleCheck);
+library.add(fab, faEnvelope, faFolder, faLaptopCode, faSquareCheck, faCircleCheck, faFileCode);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
