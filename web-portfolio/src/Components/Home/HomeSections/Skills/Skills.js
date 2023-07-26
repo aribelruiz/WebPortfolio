1175203640868cbb4aa99a26928cfcf83cc62c25
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-07-26 09:55:00 -0400

    Added skills transition

--- web-portfolio/src/Components/Home/HomeSections/Skills/Skills.js
+++ web-portfolio/src/Components/Home/HomeSections/Skills/Skills.js
@@ -24,15 +24,43 @@ import unityImg from '../../../../Images/Icons/unity.png';
 const Skills = () => {
 
     const [toggleSkills, setToggleSkills] = useState(true);
-    
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const ref = useRef(null);
+
+
+    // Initializes observer reference for skill animation
+    useEffect(() => {
+
+        const observer = new IntersectionObserver(([entry]) => {
+            setIsIntersecting(entry.isIntersecting);
+        });
+
+        console.log(isIntersecting);
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, []);
+
+    // When element comes into view, display element animation
+    useEffect(() => {
+        if(isIntersecting){
+            ref.current.querySelectorAll('div').forEach((el) => {
+                el.classList.add('skill-show');
+            });
+        }
+        else {
+            ref.current.querySelectorAll('div').forEach((el) => {
+                el.classList.remove('skill-show');
+            });
+        }
+    }, [isIntersecting]);
+
     return (
         <>
             <div className='home-div skills' id='skills'>
                 <div className='skills-header skills-underline'>
-                    {/* <h1 className='heading'> Skills </h1>
-                    <h1 className='subheading'> - </h1> */}
                     <h1 className='heading header-underline'> Technical Skills</h1>
                 </div>
+                
                 <div className='skills-body'>
                     <div id='skills-btns'>
                         <button className={!toggleSkills? 'skills-toggle-btn': 'toggle-active'} onClick={() => setToggleSkills(!toggleSkills)}> 
@@ -45,92 +73,75 @@ const Skills = () => {
                         </button>
                     </div>
 
-                    {/* Front End Skills */}
-                    { toggleSkills && <div className='skills-item'>
-                        {/* <div className='skills-item-header'>
-                            <FontAwesomeIcon className='skills-icon' icon={['fas', 'laptop-code']}/>
-                            <h2 style={{fontWeight: "600"}} className='subheading'> Front-End </h2>
-                        </div> */}
-                        {/* <br></br> */}
-                        <div className='skills-item-body'>                 
-                            {/* <h5 style={{textAlign:'left'}}>Languages</h5> */}
-                            <ul className='skills-list'>
-                                <div className='skill skill-icon skill-show'> 
+                    <div className='skills-item' ref={ref}>
+                        <div className='skills-item-body skill-hide'>      
+
+                            {/* Front End Skills */}           
+                            {toggleSkills && <ul className='skills-list'>
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={jsImg} alt='javascript-logo'></img>
                                     <h6 className='subheading'> JavaScript </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={tsImg} alt='typescript-logo'></img>
                                     <h6 className='subheading'> TypeScript </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={htmlImg} alt='html-logo'></img>
                                     <h6 className='subheading'> HTML </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={cssImg} alt='css-logo'></img>
                                     <h6 className='subheading'> CSS </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={reactImg} alt='react-logo'></img>
                                     <h6 className='subheading'> React </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={bootstrapImg} alt='bootstrap-logo'></img>
                                     <h6 className='subheading'> Bootstrap </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={figmaImg} alt='figma-logo'></img>
                                     <h5 className='subheading'> Figma </h5>
                                 </div>
-                            </ul>
-                        </div>
-                    </div>}
-                 
-                    {/* Software Development Skills */}
-                    { !toggleSkills && <div className='skills-item'>
-                        {/* <div className='skills-item-header'>
-                            <FontAwesomeIcon className='skills-icon' icon={['fas', 'file-code']}/>
-                            <h2 style={{fontWeight: "600"}} className='subheading'> Software Development </h2>
-                        </div> */}
-                        {/* <br></br> */}
-                        <div className='skills-item-body'>                 
-                            {/* <h5 style={{textAlign:'left'}}>Languages</h5> */}
-                                <ul className='skills-list'>
-                                <div className='skill skill-icon skill-show'> 
+                            </ul>}
+
+                            {/* Software Development Skills */}
+                            {!toggleSkills && <ul className='skills-list'>
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={javaImg} alt='java-logo'></img>
                                     <h6 className='subheading'> Java </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={cImg} alt='c-file-icon'></img>
                                     <h6 className='subheading'> C </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={cplusImg} alt='c++-logo'></img>
                                     <h6 className='subheading'> C++ </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={gitImg} alt='github-logo'></img>
                                     <h6 className='subheading'> GitHub </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={vsImg} alt='VScode-logo'></img>
                                     <h6 className='subheading'> VS Code </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={unityImg} alt='unity-logo'></img>
                                     <h6 className='subheading'> Unity </h6>
                                 </div>
-                                <div className='skill skill-icon skill-show'> 
+                                <div className='skill skill-icon'> 
                                     <img className='icon-img' src={plasticImg} alt='plastic-logo'></img>
                                     <h6 className='subheading'> plasticScm </h6>
                                 </div>
-                            </ul>
+                            </ul>}
+
                         </div>
-                    </div>}
-                
-
-
+                    </div>
                 </div>
             </div>
         </>
