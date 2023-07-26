bfe06567452542ab4845ede69131157b6295f0ec
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-07-26 10:06:51 -0400

    Added projects transition

--- web-portfolio/src/Components/Home/HomeSections/ProjectsHome.js
+++ web-portfolio/src/Components/Home/HomeSections/ProjectsHome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef} from "react";
 
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -29,18 +29,47 @@ const Projects = () => {
     const [currentProject, setCurrentProject] = useState(emptyProject);
     const handleClose = () => setShow(false);
 
+    // For handling animation
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const ref = useRef(null);
+
     // Splitting Project list to first 3 projects
     let projectHomeList = ProjectList;
     if (ProjectList.length > 3) {
         projectHomeList = ProjectList.slice(0, 3);
     }
 
-
     function setProject(currentProject) {
         setCurrentProject(currentProject);
         setShow(true);    
     }
  
+
+    // Initializes observer reference for skill animation
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            setIsIntersecting(entry.isIntersecting);
+        });
+
+        console.log(isIntersecting);
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, []);
+
+    // When element comes into view, display element animation
+    useEffect(() => {
+        if(isIntersecting){
+            ref.current.querySelectorAll('div').forEach((el) => {
+                el.classList.add('project-show');
+            });
+        }
+        else {
+            ref.current.querySelectorAll('div').forEach((el) => {
+                el.classList.remove('project-show');
+            });
+        }
+    }, [isIntersecting]);
+
 
     return (
         <div className="home-div projects" id='projects'>
@@ -49,9 +78,9 @@ const Projects = () => {
                     <h1 className="heading header-underline">My Personal Projects</h1>
                 </div>
                 <div className="projects-body">
-                <div className="project-list">
+                <div className="project-list" ref={ref}>
                         {projectHomeList.map((project, index) => 
-                            <div key={index} className='project-card-btn' onClick={() => setProject(project)}>
+                            <div key={index} className='project-card-btn project-hide' onClick={() => setProject(project)}>
                                 <ProjectCard name={project.name} image={project.img} tagline={project.tagline}/>
                             </div>
                         )}
