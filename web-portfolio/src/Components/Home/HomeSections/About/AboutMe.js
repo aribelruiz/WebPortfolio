84929884fcda7c3aadbe8fc457f7e5141187cab6
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-08-23 12:52:48 -0400

    Updated CSS for about me

--- web-portfolio/src/Components/Home/HomeSections/About/AboutMe.js
+++ web-portfolio/src/Components/Home/HomeSections/About/AboutMe.js
@@ -18,8 +18,10 @@ const AboutMe = () => {
                         <h1 className='heading'>About Me</h1>
                         <h3 className='subheading'>Aribel Ruiz - Software Developer</h3>
                         <br></br>
-                        <h5 className='txt-body'> I am a Computer Science graduate with a minor in Secure Computing & Networks (SCAN) and a former Undergraduate Learning Assistant from the University of Central Florida.</h5>
-                        <h5 className='txt-body'> Currently seeking employment with an interest in software engineering and application/front-end development. </h5>
+                        <h5 className='txt-body'> I am a recent Computer Science graduate with a minor in Secure Computing & Networks (SCAN) and a former Undergraduate Learning Assistant from the University of Central Florida. </h5>
+                        <h5 className='txt-body'> I enjoy discovering creative solutions, ensuring reliable user experiences, and collaborating with other developers who share my growth mindset. </h5>
+                        <br></br>
+                        <h5 className='txt-body'> Currently seeking a position where I can continue pursuing my interest in software engineering and/or front-end development. </h5>
                     </div>
                 </div>
             </div>
